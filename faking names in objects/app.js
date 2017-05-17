57c7d8caba836fcbad9d82c9cbd65ca19900e817
budaman <budamantvydas@gmail.com>
2017-05-17 16:34:24 +0300

    tiesiog pavyzdziai, kodel apsimoka rasyti i objektus - nes nesipina kintamieji tada - easy

---
+++ faking names in objects/app.js
@@ -0,0 +1,18 @@
+var english = {};
+var spanish = {};
+
+
+
+
+english.greetings = {}
+english.greetings.greet = "Hello";
+spanish.greet = "Hola";
+
+
+console.log(english.greetings.greet);
+console.log(spanish.greet);
+
+
+//Tiek ispanu tiek aanglu kalba tures tuos pacius kintamuosius, norint kad jie nesusipintu
+//ir iskelti viska i globalines reiksmes, geriausia pasidaryti objektus kiekvienam, tada
+//bus aiskesnis priemas su tom pacio reiksmemis
