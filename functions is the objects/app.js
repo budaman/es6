1a67c148f766da992a087ddb24ee43f3a95b2998
budaman <budamantvydas@gmail.com>
2017-05-17 17:04:30 +0300

    funkcijos yra tas pats kaip ir objektai, todel joms galima priskirti properties kaip ir objektams ir nebutinai jas issaukti, galima is consol loginti ar panasiai

---
+++ functions is the objects/app.js
@@ -0,0 +1,9 @@
+function greet() {
+   console.log('hi');
+}
+
+
+greet.language = "English";
+
+console.log(greet.language); //function yra objektas, todel kaip ir visiem objectam,
+//pridejau property (savybe) siaip ziauriai keista ir kitose kalbose to nera
